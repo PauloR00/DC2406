b94b99a7e0ba6203baf27b44e47b47d83c354579
PAULO RENATO ROCHA CAVALCANTE FILHO <paulofilho0956@digitalcollege.internal>
2025-06-24 21:58:17 -0300

    consultando cpf

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const consultarCep = () => {
+    const cep = document.getElementById('cep').value
+    
+
+    let uri = `https://cep.awesomeapi.com.br/json/${cep}`
+    
+    console.log(`URI: ${uri}`)
+    fetch(uri)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+    
+    
+    
+    
+        document.getElementById('log').value = json.address
+    })
+
+}
