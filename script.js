141a56b36821a052ef8e62cedc2d2642d9c1b25e
PAULO RENATO ROCHA CAVALCANTE FILHO <paulofilho0956@digitalcollege.internal>
2025-07-01 21:21:17 -0300

    .

--- script.js
+++ script.js
@@ -2,21 +2,62 @@
 
 
 const consultarCep = () => {
-    const cep = document.getElementById('cep').value
-    
+  const cep = document.getElementById('cep').value
 
-    let uri = `https://cep.awesomeapi.com.br/json/${cep}`
-    
-    console.log(`URI: ${uri}`)
-    fetch(uri)
-      .then(response => response.json())
-      .then(json => {
-        console.log(json)
-    
-    
-    
-    
-        document.getElementById('log').value = json.address
+
+  let uri = `https://cep.awesomeapi.com.br/json/${cep}`
+
+  console.log(`URI: ${uri}`)
+  fetch(uri)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+
+
+
+
+      document.getElementById('log').value = json.address
+      document.getElementById('')
     })
 
+}
+
+const fetchEstados = () => {
+  let uriEstados = `https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome`
+
+  console.log(`URI: ${uriEstados}`)
+
+  fetch(uriEstados)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      let options = '<option>Selecione...</option>'
+
+      data.forEach(estado => {
+        options = options + `<option value = "${estado.sigla}">${estado.nome}</option>`
+
+      });
+
+      document.getElementById('uf').innerHTML = options
+
+    })
+}
+
+fetchEstados()
+
+const fetchMunicipios = () => {
+  fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`)
+    .then(responde => responde.json())
+    .then(json =>{
+      console.log(json)
+
+
+      let options = '<options>Selecione...</options>'
+
+      json.forEach(municipio =>{
+        options = options = `<options value ="${municipio.sigla}">${municipio.nome}`
+      })
+
+    })
 }
